58b3be1789948dd7eaeb41993393815caff8bc09
Hakan Tandogan <hakan@gurkensalat.com>
2014-07-15 12:50:45 +0200

    Additional environment variable for TMP directory

--- extract-places-osm.sh
+++ extract-places-osm.sh
@@ -2,7 +2,7 @@
 
 OSMOSIS=${HOME}/osmosis-0.43.1
 STORAGE=${HOME}/Dropbox/osmdata
-TMPDIR=${HOME}/tmp/osm-place-connectivity
+TMPDIR=${HOME}/tmp/osm-place-mosques
 WEBDATA=/home/tomcat/osm-mosques/data
 
 COUNTIES=
