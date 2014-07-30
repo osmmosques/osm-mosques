4d7691bd1f8379715fa2a52094f0b46990836083
Hakan Tandogan <hakan@gurkensalat.com>
2014-07-30 23:53:28 +0200

    Load basic mosque places from german counties

--- webapp-rest/src/main/java/com/gurkensalat/osm/mosques/OsmController.java
+++ webapp-rest/src/main/java/com/gurkensalat/osm/mosques/OsmController.java
@@ -7,7 +7,6 @@ import com.gurkensalat.osm.entity.Place;
 import com.gurkensalat.osm.entity.PlaceType;
 import com.gurkensalat.osm.repository.OsmRepository;
 import com.gurkensalat.osm.repository.PlaceRepository;
-import org.apache.commons.digester3.Digester;
 import org.apache.commons.lang3.CharEncoding;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -21,13 +20,11 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
-import org.xml.sax.SAXException;
 
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.net.URLDecoder;
+import java.util.Arrays;
 import java.util.List;
 
 @RestController
@@ -39,6 +36,26 @@ public class OsmController
     private final static String REQUEST_ROOT = "/osm";
 
     private final static String REQUEST_IMPORT = REQUEST_ROOT + "/import";
+
+    private final static List<String> germanCounties =
+            Arrays.asList(
+                    "baden-wuerttemberg",
+                    "bayern",
+                    "berlin",
+                    "brandenburg",
+                    "bremen",
+                    "hamburg",
+                    "hessen",
+                    "mecklenburg-vorpommern",
+                    "niedersachsen",
+                    "nordrhein-westfalen",
+                    "rheinland-pfalz",
+                    "saarland",
+                    "sachsen-anhalt",
+                    "sachsen",
+                    "schleswig-holstein",
+                    "thueringen"
+            );
 
     @Autowired
     private OsmRepository osmRepository;
@@ -84,64 +101,74 @@ public class OsmController
             dataDirectory = new File(dataDirectory, path);
         }
 
-        // TODO sanitize data directory first...
-        File dataFile = new File(dataDirectory, "turkey-places-city.osm");
+        LOGGER.info("OSM Repository is: {}", osmRepository);
 
-        LOGGER.info("OSM Repository was: {}", osmRepository);
+        LOGGER.debug("Place Repository is: {}", placeRepository);
 
-        OsmRoot root = osmRepository.parse(dataFile);
+        // Empty the storage first
+        placeRepository.deleteAll();
 
-        LOGGER.info("Place Repository is: {}", placeRepository);
+        LOGGER.info("Data Directory is {}", dataDirectory.getAbsolutePath());
 
-        for (OsmNode node : root.getNodes())
+        for (String county : germanCounties)
         {
-            LOGGER.info("Read node: {}, {}, {}", node, node.getLat(), node.getLon());
+            // TODO sanitize data directory first...
+            File dataFile = new File(dataDirectory, "germany-" + county + "-religion-muslim" + ".osm");
 
-            // re-create a place from OSM data
-            Place tempPlace = new Place(null, PlaceType.OSM_CITY);
-            tempPlace.setLat(node.getLat());
-            tempPlace.setLon(node.getLon());
+            OsmRoot root = osmRepository.parse(dataFile);
 
-            for (OsmTag tag : node.getTags())
+            LOGGER.info("Read {} nodes from {}", root.getNodes().size(), dataFile.getName());
+
+            for (OsmNode node : root.getNodes())
             {
-                String key = tag.getKey().toLowerCase();
-                String val = tag.getValue();
-                LOGGER.info("    '{}' -> '{}'", key, val);
+                LOGGER.info("Read node: {}, {}, {}", node, node.getLat(), node.getLon());
 
-                if ("name".equals(key))
-                {
-                    tempPlace.setName(val);
-                }
-            }
+                // re-create a place from OSM data
+                Place tempPlace = new Place(null, PlaceType.OSM_PLACE_OF_WORSHIP);
+                tempPlace.setLat(node.getLat());
+                tempPlace.setLon(node.getLon());
 
-            // Now, insert-or-update the place
-            try
-            {
-                Place place;
-                List<Place> places = placeRepository.findByNameAndType(tempPlace.getName(), tempPlace.getType());
-                if ((places == null) || (places.size() == 0))
+                for (OsmTag tag : node.getTags())
                 {
-                    // Place could not be found, insert it...
-                    place = placeRepository.save(tempPlace);
+                    String key = tag.getKey().toLowerCase();
+                    String val = tag.getValue();
+                    LOGGER.info("    '{}' -> '{}'", key, val);
+
+                    if ("name".equals(key))
+                    {
+                        tempPlace.setName(val);
+                    }
                 }
-                else
+
+                // Now, insert-or-update the place
+                try
                 {
-                    // take the one from the database and update it
-                    place = places.get(0);
-                    place = placeRepository.findOne(place.getId());
+                    Place place;
+                    List<Place> places = placeRepository.findByNameAndType(tempPlace.getName(), tempPlace.getType());
+                    if ((places == null) || (places.size() == 0))
+                    {
+                        // Place could not be found, insert it...
+                        place = placeRepository.save(tempPlace);
+                    }
+                    else
+                    {
+                        // take the one from the database and update it
+                        place = places.get(0);
+                        place = placeRepository.findOne(place.getId());
+                    }
+
                     place.setLat(tempPlace.getLat());
                     place.setLon(tempPlace.getLon());
                     place.setType(tempPlace.getType());
                     place = placeRepository.save(place);
-                }
 
-                LOGGER.info("Saved Place {}", place);
-            }
-            catch (Exception e)
-            {
-                LOGGER.info("While persisting place", e);
-                LOGGER.info("Place:");
-                LOGGER.info("    name: '{}'", tempPlace.getName());
+                    LOGGER.info("Saved Place {}", place);
+                }
+                catch (Exception e)
+                {
+                    LOGGER.error("While persisting place", e);
+                    LOGGER.info("Place: {}", tempPlace);
+                }
             }
         }
 
