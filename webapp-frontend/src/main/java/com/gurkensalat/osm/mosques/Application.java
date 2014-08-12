ebd9c89d434e8081f389c5919f51d007ac8b0455
Hakan Tandogan <hakan@gurkensalat.com>
2014-08-12 19:39:04 +0200

    I18N support through Spring

--- webapp-frontend/src/main/java/com/gurkensalat/osm/mosques/Application.java
+++ webapp-frontend/src/main/java/com/gurkensalat/osm/mosques/Application.java
@@ -1,22 +1,26 @@
 package com.gurkensalat.osm.mosques;
 
-import com.gurkensalat.osm.entity.DitibPlace;
 import com.gurkensalat.osm.entity.EntityComponentScanMarker;
 import com.gurkensalat.osm.repository.RepositoryComponentScanMarker;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.orm.jpa.EntityScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.annotation.PropertySources;
+import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.data.rest.webmvc.config.RepositoryRestMvcConfiguration;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.i18n.AcceptHeaderLocaleResolver;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 
 @Configuration
 @PropertySources({
@@ -29,7 +33,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @ComponentScan(basePackageClasses = {EntityComponentScanMarker.class, RepositoryComponentScanMarker.class, ApplicationComponentScanMarker.class})
 @EnableAutoConfiguration
 @Controller
-public class Application
+public class Application extends WebMvcConfigurerAdapter
 {
     public static void main(String[] args)
     {
@@ -37,6 +41,37 @@ public class Application
 
         app.setShowBanner(false);
         app.run(args);
+    }
+
+    @Bean
+    public LocaleResolver localeResolver()
+    {
+        AcceptHeaderLocaleResolver localeResolver = new AcceptHeaderLocaleResolver();
+        // SessionLocaleResolver slr = new SessionLocaleResolver();
+        // slr.setDefaultLocale(Locale.US);
+        return localeResolver;
+    }
+
+    @Bean
+    public LocaleChangeInterceptor localeChangeInterceptor()
+    {
+        LocaleChangeInterceptor lci = new LocaleChangeInterceptor();
+        return lci;
+    }
+
+    @Bean
+    public ResourceBundleMessageSource messageSource()
+    {
+        ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+        messageSource.setBasename("i18n/messages");
+        messageSource.setFallbackToSystemLocale(false);
+        return messageSource;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry)
+    {
+        registry.addInterceptor(localeChangeInterceptor());
     }
 
     @RequestMapping("/")
