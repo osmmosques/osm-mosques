00f8bedcb1106e7fc010d3ceba01b8d2aaa371dc
Hakan Tandogan <hakan@gurkensalat.com>
2014-08-13 21:47:15 +0200

    Remove any leftovers of webapp split

--- webapp-rest/src/main/java/com/gurkensalat/osm/mosques/ApplicationComponentScanMarker.java
+++
@@ -1,8 +0,0 @@
-package com.gurkensalat.osm.mosques;
-
-/**
- * Empty marker interface to make this package visible to component scanners.
- */
-public interface ApplicationComponentScanMarker
-{
-}
