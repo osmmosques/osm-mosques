75132844c0fb28fe94441135da6f7106b588dcc1
Hakan Tandogan <hakan@gurkensalat.com>
2014-08-18 21:52:17 +0200

    Further mocking...

--- combined-persistence/src/main/java/com/gurkensalat/osm/repository/CombinedScoreCalculatorImpl.java
+++ combined-persistence/src/main/java/com/gurkensalat/osm/repository/CombinedScoreCalculatorImpl.java
@@ -1,5 +1,40 @@
 package com.gurkensalat.osm.repository;
 
-public interface CombinedScoreCalculatorImpl
+import com.gurkensalat.osm.entity.CombinedPlace;
+import com.gurkensalat.osm.entity.DitibPlace;
+import com.gurkensalat.osm.entity.OsmPlace;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CombinedScoreCalculatorImpl implements CombinedScoreCalculator
 {
+    @Override
+    public void calculateAllScores()
+    {
+
+    }
+
+    @Override
+    public void calculateAllDitibScores()
+    {
+
+    }
+
+    @Override
+    public void calculateAllOSMScores()
+    {
+
+    }
+
+    @Override
+    public void calculateDitibScore(CombinedPlace place, DitibPlace ditibPlace)
+    {
+
+    }
+
+    @Override
+    public void calculateOSMScore(CombinedPlace place, OsmPlace osmPlace)
+    {
+
+    }
 }
