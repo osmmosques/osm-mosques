ec3c87b7fbd225b3726639f22b9041c4345dcd9e
Hakan Tandogan <hakan@gurkensalat.com>
2014-08-21 19:40:36 +0200

    Actually call the geocoding service

--- webapp/src/main/resources/static/js/osm-mosques.js
+++ webapp/src/main/resources/static/js/osm-mosques.js
@@ -4,5 +4,20 @@
 
 function attemptGeocode(ditibKey)
 {
-    alert("Hello World - >" + ditibKey + "<");
+    // http://localhost:8888/rest/ditib/geocode/84030-20-5
+    var gcUrl = "../rest/ditib/geocode/" + ditibKey;
+    console.log(gcUrl);
+    $.getJSON(gcUrl, function (data)
+    {
+        console.log(data);
+        if (data["geocoded"] == true)
+        {
+            alert("YAY!");
+        }
+        else
+        {
+            alert("booooooh");
+        }
+        // JSON.stringify(data)
+    });
 }
