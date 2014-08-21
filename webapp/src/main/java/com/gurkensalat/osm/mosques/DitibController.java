412c294cef4a14a6777f0761592c841ba0562188
Hakan Tandogan <hakan@gurkensalat.com>
2014-08-21 18:47:27 +0200

    Split up ditib list by geocoding status

--- webapp/src/main/java/com/gurkensalat/osm/mosques/DitibController.java
+++ webapp/src/main/java/com/gurkensalat/osm/mosques/DitibController.java
@@ -11,6 +11,9 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
 @EnableAutoConfiguration
 public class DitibController
@@ -25,12 +28,54 @@ public class DitibController
     @RequestMapping(value = REQUEST_ROOT + "/list", method = RequestMethod.GET)
     public String ditibList(Model model)
     {
-        LOGGER.info("ditibPlaceRepository is {}", ditibPlaceRepository);
-
         model.addAttribute("repository", ditibPlaceRepository);
 
         Iterable<DitibPlace> places = ditibPlaceRepository.findAll();
         model.addAttribute("places", places);
+
+        return "ditib-list";
+    }
+
+    @RequestMapping(value = REQUEST_ROOT + "/list-already-geocoded", method = RequestMethod.GET)
+    public String ditibListAlreadyGeocoded(Model model)
+    {
+        model.addAttribute("repository", ditibPlaceRepository);
+
+        Iterable<DitibPlace> places = ditibPlaceRepository.findAll();
+
+        // Actually, investigate FluentIterable here...
+        List<DitibPlace> alreadyGeocodedPlaces = new ArrayList();
+        for (DitibPlace place : places)
+        {
+            if (place.isGeocoded())
+            {
+                alreadyGeocodedPlaces.add(place);
+            }
+        }
+
+        model.addAttribute("places", alreadyGeocodedPlaces);
+
+        return "ditib-list";
+    }
+
+    @RequestMapping(value = REQUEST_ROOT + "/list-to-be-geocoded", method = RequestMethod.GET)
+    public String ditibListToBeGeocoded(Model model)
+    {
+        model.addAttribute("repository", ditibPlaceRepository);
+
+        Iterable<DitibPlace> places = ditibPlaceRepository.findAll();
+
+        // Actually, investigate FluentIterable here...
+        List<DitibPlace> toBeGeocodedPlaces = new ArrayList();
+        for (DitibPlace place : places)
+        {
+            if (!(place.isGeocoded()))
+            {
+                toBeGeocodedPlaces.add(place);
+            }
+        }
+
+        model.addAttribute("places", toBeGeocodedPlaces);
 
         return "ditib-list";
     }
