5a333ff7bccdc087c2c6aaafe80a5a8cbfadf137
Hakan Tandogan <hakan@gurkensalat.com>
2014-08-22 18:05:00 +0200

    WIP - List of place to be qa-ed

---
+++ webapp/src/main/java/com/gurkensalat/osm/mosques/QaDataController.java
@@ -0,0 +1,90 @@
+package com.gurkensalat.osm.mosques;
+
+import com.gurkensalat.osm.entity.DitibPlace;
+import com.gurkensalat.osm.entity.LinkedPlace;
+import com.gurkensalat.osm.entity.OsmPlace;
+import com.gurkensalat.osm.entity.PlaceType;
+import com.gurkensalat.osm.repository.DitibPlaceRepository;
+import com.gurkensalat.osm.repository.LinkedPlaceRepository;
+import com.gurkensalat.osm.repository.OsmPlaceRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.List;
+
+import static org.apache.commons.collections4.ListUtils.emptyIfNull;
+import static org.apache.commons.lang3.StringUtils.isNotEmpty;
+
+@Controller
+@EnableAutoConfiguration
+public class QaDataController
+{
+    private final static Logger LOGGER = LoggerFactory.getLogger(QaDataController.class);
+
+    private final static String REQUEST_ROOT = "/qadata";
+
+    @Autowired
+    private LinkedPlaceRepository linkedPlaceRepository;
+
+    @Autowired
+    private DitibPlaceRepository ditibPlaceRepository;
+
+    @Autowired
+    private OsmPlaceRepository osmPlaceRepository;
+
+    @RequestMapping(value = REQUEST_ROOT + "/list", method = RequestMethod.GET)
+    public String linkedPlaceList(Model model)
+    {
+        LOGGER.info("osmPlaceRepository is {}", linkedPlaceRepository);
+
+        Iterable<LinkedPlace> places = linkedPlaceRepository.findAll();
+
+        for (LinkedPlace linkedPlace : places)
+        {
+            String key = linkedPlace.getDitibCode();
+            if (isNotEmpty(key))
+            {
+                List<DitibPlace> ditibPlaces = emptyIfNull(ditibPlaceRepository.findByKey(key));
+                DitibPlace place;
+                if (ditibPlaces.size() > 0)
+                {
+                    place = ditibPlaces.get(0);
+                }
+                else
+                {
+                    place = new DitibPlace(key);
+                }
+
+                linkedPlace.setDitibPlace(place);
+            }
+
+
+            key = linkedPlace.getOsmId();
+            if (isNotEmpty(key))
+            {
+                List<OsmPlace> osmPlaces = emptyIfNull(osmPlaceRepository.findByKey(key));
+                OsmPlace place;
+                if (osmPlaces.size() > 0)
+                {
+                    place = osmPlaces.get(0);
+                }
+                else
+                {
+                    place = new OsmPlace("no-name", PlaceType.OSM_CITY);
+                }
+
+                linkedPlace.setOsmPlace(place);
+            }
+        }
+
+        model.addAttribute("places", places);
+
+        return "linked-place-list";
+    }
+}
