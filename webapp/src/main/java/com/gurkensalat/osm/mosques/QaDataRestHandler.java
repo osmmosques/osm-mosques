e333149ac6f652d8ad03f1c483ada3cb0b288d05
Hakan Tandogan <hakan@gurkensalat.com>
2014-08-23 13:43:26 +0200

    Use GreatCircle distance to find OSM places nearby

--- webapp/src/main/java/com/gurkensalat/osm/mosques/QaDataRestHandler.java
+++ webapp/src/main/java/com/gurkensalat/osm/mosques/QaDataRestHandler.java
@@ -7,6 +7,8 @@ import com.gurkensalat.osm.repository.DitibPlaceRepository;
 import com.gurkensalat.osm.repository.LinkedPlaceRepository;
 import com.gurkensalat.osm.repository.OsmPlaceRepository;
 import com.gurkensalat.osm.repository.QaScoreCalculator;
+import com.gurkensalat.osm.utils.GreatCircle;
+import org.apache.commons.collections4.ListUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,6 +19,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @EnableAutoConfiguration
@@ -31,6 +35,8 @@ public class QaDataRestHandler
     private final static String REQUEST_CALCULATE_DITIB_SCORE = REQUEST_ROOT + "/calculate/ditib/{ditibCode}";
 
     private final static String REQUEST_CALCULATE_OSM_SCORE = REQUEST_ROOT + "/calculate/osm/{osmId}";
+
+    private static final double DELTA_LAT_LON_10KM = 0.1;
 
     @Autowired
     private LinkedPlaceRepository linkedPlaceRepository;
@@ -59,6 +65,13 @@ public class QaDataRestHandler
         else
         {
             LinkedPlace place = findByDitibCode(ditibCode);
+
+            OsmPlace osmPlace = findClosestByBBOX(place, DELTA_LAT_LON_10KM, DELTA_LAT_LON_10KM);
+            if (osmPlace != null)
+            {
+                place.setOsmId(Long.toString(osmPlace.getId()));
+                place.setOsmPlace(osmPlace);
+            }
 
             qaScoreCalculator.calculateDitibScore(place);
             place = linkedPlaceRepository.save(place);
@@ -118,6 +131,32 @@ public class QaDataRestHandler
         }
 
         return place;
+    }
+
+    private OsmPlace findClosestByBBOX(LinkedPlace place, double deltaLat, double deltaLon)
+    {
+        OsmPlace result = null;
+
+        List<OsmPlace> osmPlaces = osmPlaceRepository.findByBbox(
+                place.getLon() - deltaLon, place.getLat() - deltaLat,
+                place.getLon() + deltaLon, place.getLat() + deltaLat);
+
+        osmPlaces = ListUtils.emptyIfNull(osmPlaces);
+
+        double distance = 1000000000;
+        for (OsmPlace osmPlace : osmPlaces)
+        {
+            LOGGER.info("  Calculating distance for {}", osmPlace);
+            double newDistance = GreatCircle.distanceInKm(place.getLat(), place.getLon(), osmPlace.getLat(), osmPlace.getLon());
+
+            if (newDistance < distance)
+            {
+                distance = newDistance;
+                result = osmPlace;
+            }
+        }
+
+        return result;
     }
 
 }
