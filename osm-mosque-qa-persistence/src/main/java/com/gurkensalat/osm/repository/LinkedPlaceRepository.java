0568ea5aedce7b298ea157a4a7d373d702e0dc87
Hakan Tandogan <hakan@gurkensalat.com>
2014-08-24 10:23:07 +0200

    Methods to mark entities as invalid and remove them

--- osm-mosque-qa-persistence/src/main/java/com/gurkensalat/osm/repository/LinkedPlaceRepository.java
+++ osm-mosque-qa-persistence/src/main/java/com/gurkensalat/osm/repository/LinkedPlaceRepository.java
@@ -1,9 +1,13 @@
 package com.gurkensalat.osm.repository;
 
 import com.gurkensalat.osm.entity.LinkedPlace;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+import javax.transaction.Transactional;
 
 @RepositoryRestResource(collectionResourceRel = "linkedPlace", path = "linkedPlace")
 public interface LinkedPlaceRepository extends PagingAndSortingRepository<LinkedPlace, Long>
@@ -11,4 +15,14 @@ public interface LinkedPlaceRepository extends PagingAndSortingRepository<Linked
     Iterable<LinkedPlace> findAllByOsmId(@Param("OSM_ID") String osmId);
 
     Iterable<LinkedPlace> findAllByDitibCode(@Param("DITIB_CODE") String ditibCode);
+
+    @Modifying
+    @Transactional
+    @Query("update LinkedPlace set valid = 'f'")
+    void invalidateAll();
+
+    @Modifying
+    @Transactional
+    @Query("delete from LinkedPlace where valid = 'f'")
+    void deleteAllInvalid();
 }
