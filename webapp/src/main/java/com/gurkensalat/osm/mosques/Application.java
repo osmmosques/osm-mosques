348173effeca47b66d8ab2066188a159b9740900
Hakan Tandogan <hakan@gurkensalat.com>
2015-04-17 20:19:50 +0200

    Re-enable i18n support in thymeleaf templates

--- webapp/src/main/java/com/gurkensalat/osm/mosques/Application.java
+++ webapp/src/main/java/com/gurkensalat/osm/mosques/Application.java
@@ -25,16 +25,24 @@ import com.gurkensalat.osm.entity.EntityComponentScanMarker;
 import com.gurkensalat.osm.repository.RepositoryComponentScanMarker;
 import com.tandogan.geostuff.opencagedata.GeocodeRepositoryComponentScanMarker;
 import com.tandogan.geostuff.opencagedata.entity.GeocodeEntityComponentScanMarker;
+import org.apache.commons.lang3.CharEncoding;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.orm.jpa.EntityScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.annotation.PropertySources;
+import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+import java.util.Locale;
 
 @SpringBootApplication
 @Controller
@@ -53,7 +61,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 })
 @EnableJpaRepositories(basePackageClasses = {RepositoryComponentScanMarker.class})
 @EntityScan(basePackageClasses = {EntityComponentScanMarker.class})
-public class Application
+public class Application extends WebMvcConfigurerAdapter
 {
     public static void main(String[] args)
     {
@@ -61,6 +69,33 @@ public class Application
 
         app.setShowBanner(false);
         app.run(args);
+    }
+
+    @Bean
+    public LocaleResolver localeResolver()
+    {
+        // AcceptHeaderLocaleResolver localeResolver = new AcceptHeaderLocaleResolver();
+        SessionLocaleResolver localeResolver = new SessionLocaleResolver();
+        localeResolver.setDefaultLocale(Locale.US);
+        return localeResolver;
+    }
+
+    @Bean
+    public LocaleChangeInterceptor localeChangeInterceptor()
+    {
+        LocaleChangeInterceptor lci = new LocaleChangeInterceptor();
+        lci.setParamName("lang");
+        return lci;
+    }
+
+    @Bean
+    public ResourceBundleMessageSource messageSource()
+    {
+        ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+        messageSource.setBasename("i18n/messages");
+        messageSource.setFallbackToSystemLocale(false);
+        messageSource.setDefaultEncoding(CharEncoding.UTF_8);
+        return messageSource;
     }
 
     @RequestMapping("/")
