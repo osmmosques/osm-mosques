6408c6b25f52060094e67052f8fec787624cd2a8
Hakan Tandogan <hakan@tandogan.com>
2016-03-04 20:41:19 +0100

    Put all relevant initialization code in document.ready()

--- webapp/src/main/resources/static/js/osm-mosques-map.js
+++ webapp/src/main/resources/static/js/osm-mosques-map.js
@@ -1,10 +1,13 @@
+$(document).ready(init);
+
+var map;
 
 <!-- For contextmenu -->
-function showCoordinates (e) {
+function showCoordinates(e) {
     alert(e.latlng);
 }
 
-function centerMap (e) {
+function centerMap(e) {
     map.panTo(e.latlng);
 }
 
@@ -24,95 +27,98 @@ function onMapMoveEnd() {
 }
 
 <!-- Markers from here on -->
-var ditibMosqueIcon = L.MakiMarkers.icon({
-    icon: "religious-muslim",
-    color: "#00cc33",
-    size: "m"
-});
+function init() {
 
-var osmMosqueIcon = L.MakiMarkers.icon({
-    icon: "religious-muslim",
-    color: "#6699ff",
-    size: "m"
-});
+    var ditibMosqueIcon = L.MakiMarkers.icon({
+        icon: "religious-muslim",
+        color: "#00cc33",
+        size: "m"
+    });
 
-var ditibPlaces = L.markerClusterGroup({ chunkedLoading: true });
+    var osmMosqueIcon = L.MakiMarkers.icon({
+        icon: "religious-muslim",
+        color: "#6699ff",
+        size: "m"
+    });
 
-var osmPlaces = L.markerClusterGroup({ chunkedLoading: true });
+    var ditibPlaces = L.markerClusterGroup({chunkedLoading: true});
 
-for (var i = 0; i < ditibAddressPoints.length; i++) {
-    var a = ditibAddressPoints[i];
-    var title = a[2];
-    var marker = L.marker(L.latLng(a[0], a[1]), { title: title, icon: ditibMosqueIcon });
-    marker.bindPopup(title);
-    ditibPlaces.addLayer(marker);
+    var osmPlaces = L.markerClusterGroup({chunkedLoading: true});
+
+    for (var i = 0; i < ditibAddressPoints.length; i++) {
+        var a = ditibAddressPoints[i];
+        var title = a[2];
+        var marker = L.marker(L.latLng(a[0], a[1]), {title: title, icon: ditibMosqueIcon});
+        marker.bindPopup(title);
+        ditibPlaces.addLayer(marker);
+    }
+
+    for (var i = 0; i < osmAddressPoints.length; i++) {
+        var a = osmAddressPoints[i];
+        var title = a[2];
+        var marker = L.marker(L.latLng(a[0], a[1]), {title: title, icon: osmMosqueIcon});
+        marker.bindPopup(title);
+        osmPlaces.addLayer(marker);
+    }
+
+    var overlays =
+    {
+        "OSM Places": osmPlaces,
+        "DITIB Places": ditibPlaces
+    };
+
+    <!-- Map providers -->
+    var osmMapnikMap = L.tileLayer.provider('OpenStreetMap.Mapnik');
+    var stamenWatercolorMap = L.tileLayer.provider('Stamen.Watercolor');
+    var thunderforestMap = L.tileLayer.provider('Thunderforest.Landscape');
+
+    mapLink = '<a href="http://openstreetmap.org">OpenStreetMap</a>';
+
+    var googleSatMap = new L.Google('SATELLITE');
+    var googleTerrainMap = new L.Google('TERRAIN');
+    var googleHybridMap = new L.Google('HYBRID');
+
+    <!-- Now the map itself -->
+    map = L.map('map', {
+        center: [48.12955, 11.34873],
+        contextmenu: true,
+        contextmenuWidth: 140,
+        contextmenuItems: [{
+            text: 'Show coordinates', callback: showCoordinates
+        }, {
+            text: 'Center map here', callback: centerMap
+        }],
+        zoom: 11,
+        zoomControl: true,
+        layers: [osmMapnikMap, osmPlaces, ditibPlaces]
+    });
+
+    var baseLayers =
+    {
+        "OSM Mapnik": osmMapnikMap,
+        "Thunderforest": thunderforestMap,
+        "Watercolor": stamenWatercolorMap,
+        "Google Satellite": googleSatMap,
+        "Google Terrain": googleTerrainMap,
+        "Google Hybrid": googleHybridMap
+    };
+
+    map.addLayer(osmPlaces);
+    map.addLayer(ditibPlaces);
+
+    <!-- Now add the layer switcher to the map -->
+    var layers = new L.Control.Layers(baseLayers, overlays);
+    map.addControl(layers);
+    map.addControl(new L.Control.Permalink({text: 'Permalink', layers: layers}));
+
+    <!-- Location control -->
+    L.control.locate().addTo(map);
+
+    <!-- Sidebar -->
+    var sidebar = L.control.sidebar('sidebar', {position: 'left'});
+    sidebar.addTo(map);
+
+    <!-- Map move methods -->
+    map.on('load', onMapLoaded);
+    map.on('moveend', onMapMoveEnd);
 }
-
-for (var i = 0; i < osmAddressPoints.length; i++) {
-    var a = osmAddressPoints[i];
-    var title = a[2];
-    var marker = L.marker(L.latLng(a[0], a[1]), { title: title, icon: osmMosqueIcon });
-    marker.bindPopup(title);
-    osmPlaces.addLayer(marker);
-}
-
-var overlays =
-{
-    "OSM Places": osmPlaces,
-    "DITIB Places": ditibPlaces
-};
-
-<!-- Map providers -->
-var osmMapnikMap = L.tileLayer.provider('OpenStreetMap.Mapnik');
-var stamenWatercolorMap = L.tileLayer.provider('Stamen.Watercolor');
-var thunderforestMap = L.tileLayer.provider('Thunderforest.Landscape');
-
-mapLink = '<a href="http://openstreetmap.org">OpenStreetMap</a>';
-
-var googleSatMap = new L.Google('SATELLITE');
-var googleTerrainMap = new L.Google('TERRAIN');
-var googleHybridMap = new L.Google('HYBRID');
-
-<!-- Now the map itself -->
-var map = L.map('map', {
-    center: [48.12955, 11.34873],
-    contextmenu: true,
-    contextmenuWidth: 140,
-    contextmenuItems: [{
-       text: 'Show coordinates', callback: showCoordinates
-    }, {
-       text: 'Center map here', callback: centerMap
-    }],
-    zoom: 11,
-    zoomControl: true,
-    layers: [osmMapnikMap, osmPlaces, ditibPlaces]
-});
-
-var baseLayers =
-{
-    "OSM Mapnik": osmMapnikMap,
-    "Thunderforest": thunderforestMap,
-    "Watercolor": stamenWatercolorMap,
-    "Google Satellite": googleSatMap,
-    "Google Terrain": googleTerrainMap,
-    "Google Hybrid": googleHybridMap
-};
-
-map.addLayer(osmPlaces);
-map.addLayer(ditibPlaces);
-
-<!-- Now add the layer switcher to the map -->
-var layers = new L.Control.Layers(baseLayers, overlays);
-map.addControl(layers);
-map.addControl(new L.Control.Permalink({text: 'Permalink', layers: layers}));
-
-<!-- Location control -->
-L.control.locate().addTo(map);
-
-<!-- Sidebar -->
-var sidebar = L.control.sidebar('sidebar', {position: 'left'});
-sidebar.addTo(map);
-
-<!-- Map move methods -->
-map.on('load', onMapLoaded);
-map.on('moveend', onMapMoveEnd);
