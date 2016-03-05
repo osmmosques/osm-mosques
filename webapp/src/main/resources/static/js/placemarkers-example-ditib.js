44f877244f1ae239bfd4788bdff502fe47bc2085
Hakan Tandogan <hakan@tandogan.com>
2016-03-05 10:24:06 +0100

    Placemarker data is being loaded asynchronously

--- webapp/src/main/resources/static/js/placemarkers-example-ditib.js
+++
@@ -1,5 +0,0 @@
-var ditibAddressPoints = [
-    [48.960227966308594, 11.400359153747559, "KIPFENBERG / DITIB Türkisch Islamische Kultur Verein e.V."],
-    [48.18690490722656, 11.56020450592041, "MÜNCHEN / DITIB Türkisch Islamische Gemeinde zu München e.V."],
-    [46.42, 6.55, "THANNHAUSEN / Ayasofya Camii"]
-]
--- webapp/src/main/resources/static/js/placemarkers-example-osm.js
+++
@@ -1,5 +0,0 @@
-var osmAddressPoints = [
-    [48.2105019, 11.6339992, "OSM / null / Islamisches Zentrum München"],
-    [48.0832986, 11.6481221, "OSM / München / Salih Sanli Moschee"],
-    [48.1364, 11.3872928, "OSM / Germering / Germering Camii"]
-]
