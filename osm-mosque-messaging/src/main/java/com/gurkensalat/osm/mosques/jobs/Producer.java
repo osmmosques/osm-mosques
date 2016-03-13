8fcd032e1c2ad2030fccd70d0045887505304842
Hakan Tandogan <hakan@tandogan.com>
2016-03-13 16:48:51 +0100

    Feedback from system startup

--- osm-mosque-messaging/src/main/java/com/gurkensalat/osm/mosques/jobs/Producer.java
+++ osm-mosque-messaging/src/main/java/com/gurkensalat/osm/mosques/jobs/Producer.java
@@ -19,6 +19,17 @@ public class Producer
 {
     private final static Logger LOGGER = LoggerFactory.getLogger(Producer.class);
 
+    /*
+     * From Log:
+     *
+     * 2016-03-13 16:46:23,346  INFO ontext.support.DefaultLifecycleProcessor: 341 - Starting beans in phase 2147483647
+     * 2016-03-13 16:46:23,557  INFO bbit.connection.CachingConnectionFactory: 281 - Created new connection: SimpleConnection@37d89907 [delegate=amqp://hakan@127.0.0.1:5672/]
+     * 2016-03-13 16:46:23,560  INFO ngframework.amqp.rabbit.core.RabbitAdmin: 428 - Auto-declaring a non-durable, auto-delete, or exclusive Queue (spring-boot) durable:false, auto-delete:false, exclusive:false. It will be redeclared if the broker stops and is restarted while the connection factory is alive, but all messages will be lost.
+     * 2016-03-13 16:46:23,665  INFO g.apache.coyote.http11.Http11NioProtocol: 180 - Initializing ProtocolHandler ["http-nio-127.0.0.1-8888"]
+     *
+     * Look at the Auto-declaring part....
+     */
+
     @Autowired
     RabbitTemplate rabbitTemplate;
 
