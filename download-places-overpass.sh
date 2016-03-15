e0a864554d2cc89f596c2a1d3f03290c1deaf72a
Hakan Tandogan <hakan@tandogan.com>
2016-03-15 19:37:16 +0100

    This script is only about downloading data from overpass now

---
+++ download-places-overpass.sh
@@ -0,0 +1,28 @@
+#! /bin/sh
+
+set -e
+
+export HERE=$(dirname $(readlink -f $0))
+. ${HERE}/settings.sh
+
+
+cd ${TMPDIR}
+
+for type in node way relation
+do
+    WORLD_FILE=${TMPDIR}/world-religion-muslim-${type}.osm
+
+    wget "http://www.overpass-api.de/api/interpreter" \
+	--post-file=${HERE}/data/query-${type}-religion-muslim.xml \
+	-O ${WORLD_FILE} \
+	> ${WORLD_FILE}.out 2> ${WORLD_FILE}.err
+
+    MONTH=$(date +%Y%m --reference ${WORLD_FILE})
+    DAY=$(date +%Y%m%d --reference ${WORLD_FILE})
+
+    mkdir -p ${STORAGE}/world/${MONTH}/${DAY}
+
+    cp ${WORLD_FILE} ${STORAGE}/world/${MONTH}/${DAY}
+done
+
+# FINI
