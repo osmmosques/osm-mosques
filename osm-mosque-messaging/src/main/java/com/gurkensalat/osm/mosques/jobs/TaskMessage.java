8bae9cfbfaed6ae478821068ebae2fae4e655598
Hakan Tandogan <hakan@tandogan.com>
2016-04-03 20:52:52 +0200

    The generic TaskMessage holds a target channel too

--- osm-mosque-messaging/src/main/java/com/gurkensalat/osm/mosques/jobs/TaskMessage.java
+++ osm-mosque-messaging/src/main/java/com/gurkensalat/osm/mosques/jobs/TaskMessage.java
@@ -5,7 +5,19 @@ import org.apache.commons.lang3.builder.ToStringStyle;
 
 public class TaskMessage
 {
+    private String channel;
+
     private String message;
+
+    public String getChannel()
+    {
+        return channel;
+    }
+
+    public void setChannel(String channel)
+    {
+        this.channel = channel;
+    }
 
     public String getMessage()
     {
@@ -20,6 +32,7 @@ public class TaskMessage
     public String toString()
     {
         return (new ToStringBuilder(this, ToStringStyle.SHORT_PREFIX_STYLE))
+                .append("channel", channel)
                 .append("message", message)
                 .toString();
     }
