6ed32d9925d223b03fdab184121656d9ce91e463
Hakan Tandogan <hakan@tandogan.com>
2016-05-07 14:05:54 +0200

    Utility class for country bboxes

---
+++ osm-mosque-common/src/main/java/com/gurkensalat/osm/mosques/BBox.java
@@ -0,0 +1,26 @@
+package com.gurkensalat.osm.mosques;
+
+public class BBox
+{
+    //
+    // Cyprus
+    //
+    public static final double CYPRUS_MIN_LONGITUDE = 32.0;
+
+    public static final double CYPRUS_MIN_LATITUDE = 34.5;
+
+    public static final double CYPRUS_MAX_LONGITUDE = 34.7;
+
+    public static final double CYPRUS_MAX_LATITUDE = 35.8;
+
+    //
+    // Turkey
+    //
+    public static final double TURKEY_MIN_LONGITUDE = 25.0;
+
+    public static final double TURKEY_MIN_LATITUDE = 35.7;
+
+    public static final double TURKEY_MAX_LONGITUDE = 45.0;
+
+    public static final double TURKEY_MAX_LATITUDE = 42.0;
+}
