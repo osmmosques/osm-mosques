6ce0aa566228abc422cad987af4522d2ad30e49b
Hakan Tandogan <hakan@tandogan.com>
2020-05-09 00:16:01 +0200

    [map] Add multiple base layers to the map
    Signed-off-by: Hakan Tandogan <hakan@tandogan.com>

--- monolith/src/main/resources/static/js/osm-mosques-map.js
+++ monolith/src/main/resources/static/js/osm-mosques-map.js
@@ -1,34 +1,66 @@
-/*
- * Leaflet Example JS :)
- */
+//
+// we don't have jquery around just yet
+//
+// $(document).ready(init);
+//
+init();
 
-var mymap = L.map('map').setView([48.135, 11.389], 15);
+function init() {
 
-L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
-    maxZoom: 18,
-    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
-        '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
-        'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    id: 'mapbox/streets-v11',
-    tileSize: 512,
-    zoomOffset: -1
-}).addTo(mymap);
+    <!-- Map providers -->
+    const osmMapnikMapProvider = L.tileLayer.provider('OpenStreetMap.Mapnik');
 
-L.circle([48.135, 11.389], 500, {
-    color: 'red',
-    fillColor: '#f03',
-    fillOpacity: 0.1
-}).addTo(mymap).bindPopup("I am a circle.");
+    <!-- Now the map itself -->
+    map = L.map('map', {
+        center: [48.135, 11.389],
+        zoom: 15,
+        zoomControl: true,
+        layers: [osmMapnikMapProvider]
+    });
 
-L.control.locate().addTo(mymap);
+    const baseLayers = {
+        "OSM Mapnik": osmMapnikMapProvider,
+        "Thunderforest": L.tileLayer.provider('Thunderforest.Landscape'),
+        "Watercolor": L.tileLayer.provider('Stamen.Watercolor'),
+        "Esri Streetmap": L.tileLayer.provider('Esri.WorldStreetMap'),
+        "Esri Imagery": L.tileLayer.provider('Esri.WorldImagery')
+    };
 
-var popup = L.popup();
+// if (!(typeof google === 'undefined')) {
+//    baseLayers["Google Sattelite"] = new L.Google('SATELLITE');
+//    baseLayers["Google Terrain"] = new L.Google('TERRAIN');
+//    baseLayers["Google Hybrid"] = new L.Google('HYBRID');
+// }
 
-function onMapClick(e) {
-    popup
-        .setLatLng(e.latlng)
-        .setContent("You clicked the map at " + e.latlng.toString())
-        .openOn(mymap);
+// if (1 === 1) {
+// TODO find a way to hide the accessToken ...
+// const borkenMapBoxMapProvider = L.tileLayer.provider('MapBox', {
+//     id: 'mapbox/streets-v11',
+//     accessToken: 'pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw'
+// });
+// }
+
+    L.circle([48.135, 11.389], 500, {
+        color: 'red',
+        fillColor: '#f03',
+        fillOpacity: 0.1
+    }).addTo(map).bindPopup("I am a circle.");
+
+    <!-- Now add the layer switcher to the map -->
+    var layers = new L.Control.Layers(baseLayers);
+    map.addControl(layers);
+
+    <!-- Location control -->
+    L.control.locate().addTo(map);
+
+    var popup = L.popup();
+
+    function onMapClick(e) {
+        popup
+            .setLatLng(e.latlng)
+            .setContent("You clicked the map at " + e.latlng.toString())
+            .openOn(map);
+    }
+
+    map.on('click', onMapClick);
 }
-
-mymap.on('click', onMapClick);
