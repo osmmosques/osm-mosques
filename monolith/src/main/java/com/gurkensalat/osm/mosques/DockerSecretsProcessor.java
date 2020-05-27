442c3f414f91e61ecf4bd2441b73a88394102d4e
Hakan Tandogan <hakan@tandogan.com>
2020-05-27 21:31:21 +0200

    [docker] helper to inject docker secrets into the spring environment
    Signed-off-by: Hakan Tandogan <hakan@tandogan.com>

---
+++ monolith/src/main/java/com/gurkensalat/osm/mosques/DockerSecretsProcessor.java
@@ -0,0 +1,51 @@
+package com.gurkensalat.osm.mosques;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.env.EnvironmentPostProcessor;
+import org.springframework.core.env.ConfigurableEnvironment;
+import org.springframework.core.env.MapPropertySource;
+import org.springframework.util.FileCopyUtils;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Inspired by <a href="https://github.com/kwonghung-YIP/spring-boot-docker-secret">kwonghungs</a> example code
+ */
+
+@Slf4j
+public class DockerSecretsProcessor implements EnvironmentPostProcessor
+{
+    private String secretsDirectoryName = "/run/secrets";
+
+    @Override
+    public void postProcessEnvironment(ConfigurableEnvironment environment, SpringApplication application)
+    {
+        File secretsDirectory = new File(secretsDirectoryName);
+        if (secretsDirectory.isDirectory() && secretsDirectory.canExecute())
+        {
+            Map<String,Object> dockerSecrets = new HashMap<>();
+            try
+            {
+                File[] secretFiles = secretsDirectory.listFiles();
+                for (File secretFile: secretFiles)
+                {
+                    byte[] content = FileCopyUtils.copyToByteArray(secretFile);
+                    String key = secretFile.getName();
+                    key = key.toLowerCase().replaceAll("\\.", "_");
+                    dockerSecrets.put(key, content.toString());
+                }
+            }
+            catch (IOException ioe)
+            {
+                log.error("while reading docker secrets", ioe);
+            }
+
+            MapPropertySource propertySource = new MapPropertySource("docker-secrets", dockerSecrets);
+            environment.getPropertySources().addLast(propertySource);
+        }
+    }
+}
