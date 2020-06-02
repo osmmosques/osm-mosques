c639b82db538da5bf3c0fd23e6118dd698391f02
Hakan Tandogan <hakan@tandogan.com>
2020-06-02 10:13:27 +0200

    [mapbox] inject mapbox key from docker secrets into the Javascript
    Signed-off-by: Hakan Tandogan <hakan@tandogan.com>

--- monolith/src/main/java/com/gurkensalat/osm/mosques/MapController.java
+++ monolith/src/main/java/com/gurkensalat/osm/mosques/MapController.java
@@ -1,7 +1,9 @@
 package com.gurkensalat.osm.mosques;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
@@ -11,9 +13,13 @@ class MapController
 {
     private static final String REQUEST_MAP = "/";
 
+    @Value("${mapbox_token:unset}")
+    private String mapboxToken;
+
     @GetMapping(REQUEST_MAP)
-    public String map()
+    public String map(Model model)
     {
+        model.addAttribute("MAPBOX_TOKEN", mapboxToken);
         return "map";
     }
 }
